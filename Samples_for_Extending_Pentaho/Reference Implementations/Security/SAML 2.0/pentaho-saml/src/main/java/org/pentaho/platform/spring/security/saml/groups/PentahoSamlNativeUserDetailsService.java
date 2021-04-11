d65ba90800711ec48797135da8887aaba4bef8a0
Paul van Dongen <paul.vandongen@mpo.com>
2021-04-11 16:13:18 +0200

    BI-36 The SAML Attributes were not propagated to the session on second login due to erroneous caching. This has been fixed

--- Samples_for_Extending_Pentaho/Reference Implementations/Security/SAML 2.0/pentaho-saml/src/main/java/org/pentaho/platform/spring/security/saml/groups/PentahoSamlNativeUserDetailsService.java
+++ Samples_for_Extending_Pentaho/Reference Implementations/Security/SAML 2.0/pentaho-saml/src/main/java/org/pentaho/platform/spring/security/saml/groups/PentahoSamlNativeUserDetailsService.java
@@ -28,240 +28,189 @@ import org.springframework.security.saml.SAMLCredential;
 import org.springframework.security.saml.userdetails.SAMLUserDetailsService;
 
 /**
- * Checks the users attributes list, to try and fetch the roles from there
- * The user credentials are those sent within the SAML Authentication Response
+ * Checks the users attributes list, to try and fetch the roles from there The user credentials are those sent within
+ * the SAML Authentication Response
  */
 public class PentahoSamlNativeUserDetailsService implements SAMLUserDetailsService, UserDetailsService {
 
-  private static final Logger logger = LoggerFactory.getLogger( PentahoSamlNativeUserDetailsService.class );
+	private static final Logger logger = LoggerFactory.getLogger(PentahoSamlNativeUserDetailsService.class);
 
+	/**
+	 * The attribute in the users' attribute list that corresponds to our Pentaho Roles. A user may carry a multitude of
+	 * attributes ( age, email, drivers' license, list of favourite books , .. One such attribute will be a 'list of
+	 * Pentaho Roles'
+	 */
+	private String roleRelatedUserAttributeName;
 
-  /**
-   * The attribute in the users' attribute list that corresponds to our Pentaho Roles.
-   * A user may carry a multitude of attributes ( age, email, drivers' license, list of favourite books , ..
-   * One such attribute will be a 'list of Pentaho Roles'
-   * */
-  private String roleRelatedUserAttributeName;
+	/**
+	 * (optional) attribute values may hold a prefix that helps to contextualize that specific value; Example: in a
+	 * attribute such as 'List of OKTA SSO-based Application Roles' the values may be: 'Pentaho:Report Author',
+	 * 'Zendesk:CTools Support', 'Office365:Contributor C1', 'Pentaho:Authenticated', ...
+	 */
+	private String roleRelatedAttributePrefix;
 
-  /**
-   * (optional) attribute values may hold a prefix that helps to contextualize that specific value;
-   * Example: in a attribute such as 'List of OKTA SSO-based Application Roles' the values may be:
-   * 'Pentaho:Report Author',
-   * 'Zendesk:CTools Support',
-   * 'Office365:Contributor C1',
-   * 'Pentaho:Authenticated',
-   * ...
-   */
-  private String roleRelatedAttributePrefix;
+	/**
+	 * Regular expression which defines which attributes from the SAML credentials will be registered in the user's
+	 * session.
+	 */
+	private String sessionAttributePattern;
 
-  /**
-   * Regular expression which defines which attributes from the SAML credentials
-   * will be registered in the user's session.
-   */
-  private String sessionAttributePattern;
+	ITenantedPrincipleNameResolver tenantedPrincipleNameResolver;
 
-  ITenantedPrincipleNameResolver tenantedPrincipleNameResolver;
+	public String getRoleRelatedAttributePrefix() {
+		return roleRelatedAttributePrefix;
+	}
 
-  @Override
-  public Object loadUserBySAML( SAMLCredential credential ) throws UsernameNotFoundException {
+	public String getRoleRelatedUserAttributeName() {
+		return roleRelatedUserAttributeName;
+	}
 
-    if( credential == null || credential.getNameID() == null || credential.getNameID().getValue() == null ){
-      throw new UsernameNotFoundException( "invalid/null SAMLCredential" );
-    }
+	public String getSessionAttributePattern() {
+		return sessionAttributePattern;
+	}
 
-    String username = credential.getNameID().getValue();
+	public ITenantedPrincipleNameResolver getTenantedPrincipleNameResolver() {
+		return tenantedPrincipleNameResolver;
+	}
 
-    try {
-      // check the userDetailsMap for a UserDetails stored for this username. If we have one already, use it
-      return loadUserByUsername( username );
-    } catch ( UsernameNotFoundException usernameNotFoundException ) {
+	@Override
+	public Object loadUserBySAML(SAMLCredential credential) throws UsernameNotFoundException {
+		if (credential != null && credential.getNameID() != null && credential.getNameID().getValue() != null) {
+			String username = credential.getNameID().getValue();
+			logger.debug(String.format("Setting up SAML user details for user [%s]", username));
+			Collection<GrantedAuthority> authorities = new ArrayList();
+			authorities.add(Utils.getDefaultRole());
+			if (this.getRoleRelatedUserAttributeName() != null && !this.getRoleRelatedUserAttributeName().isEmpty()
+					&& credential.getAttribute(this.getRoleRelatedUserAttributeName()) != null) {
+				authorities.addAll(this.roleRelatedAttributeToGrantedAuthorities(credential.getAttribute(this.getRoleRelatedUserAttributeName())));
+			}
 
-      // no UserDetails found = new user coming in, create a UserDetails for it and store it in the userDetailsMap
+			UserDetails userDetails = new User(username, "ignored", true, true, true, true, authorities);
+			Utils.getUserMap().put(username, userDetails);
+			if (this.getSessionAttributePattern() != null && !this.getSessionAttributePattern().isEmpty()) {
+				this.loadSessionAttributes(PentahoSessionHolder.getSession(), credential.getAttributes(), Pattern.compile(this.getSessionAttributePattern()),
+						this.getRoleRelatedUserAttributeName());
+			}
 
-      Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+			logger.debug(String.format("Attached SAML attributes to session[%s] for user [%s]", PentahoSessionHolder.getSession().getId(), username));
+			return userDetails;
+		} else {
+			throw new UsernameNotFoundException("invalid/null SAMLCredential");
+		}
+	}
 
-      // Add the defaultRole. default role is usually "Authenticated"
-      authorities.add( Utils.getDefaultRole() );
+	@Override
+	public UserDetails loadUserByUsername(String user) throws UsernameNotFoundException {
 
-      // iterate the users attributes: if there's an attribute with the name 'getRoleRelatedUserAttributeName()',
-      // iterate through its values' list and add those to the granted authorities list
-      if( getRoleRelatedUserAttributeName() != null
-          && !getRoleRelatedUserAttributeName().isEmpty()
-          && credential.getAttribute( getRoleRelatedUserAttributeName() ) != null ){
+		if (user != null) {
 
-        authorities.addAll( roleRelatedAttributeToGrantedAuthorities( credential.getAttribute( getRoleRelatedUserAttributeName() ) ) );
-      }
+			if (getTenantedPrincipleNameResolver() != null) {
+				user = getTenantedPrincipleNameResolver().getPrincipleName(user);
+			}
 
-      // create the UserDetails object
+			UserDetails userDetails = Utils.getUserMap().get(user);
+			if (userDetails != null) {
+				return userDetails;
+			}
+		}
+		throw new UsernameNotFoundException(user);
+	}
 
-      UserDetails userDetails = new User(
-          username,
-          "ignored" /* password */,
-          true /* isEnabled */,
-          true /* isAccountNonExpired */,
-          true /* isCredentialsNonExpired */,
-          true /* isAccountNonExpired */,
-          authorities );
+	public void setRoleRelatedAttributePrefix(String roleRelatedAttributePrefix) {
+		this.roleRelatedAttributePrefix = roleRelatedAttributePrefix;
+	}
 
-      Utils.getUserMap().put( username, userDetails );
+	public void setRoleRelatedUserAttributeName(String roleRelatedUserAttributeName) {
+		this.roleRelatedUserAttributeName = roleRelatedUserAttributeName;
+	}
 
-      if ( getSessionAttributePattern() != null && !getSessionAttributePattern().isEmpty() ) {
-        loadSessionAttributes( PentahoSessionHolder.getSession(), credential.getAttributes(),
-            Pattern.compile(getSessionAttributePattern()), getRoleRelatedUserAttributeName() );
-      }
+	public void setSessionAttributePattern(String aSessionAttributePattern) {
+		sessionAttributePattern = aSessionAttributePattern;
+		if (sessionAttributePattern != null && !sessionAttributePattern.isEmpty()) {
+			try {
+				Pattern.compile(sessionAttributePattern);
+			} catch (PatternSyntaxException e) {
+				throw new IllegalArgumentException("Not a valid session attribute pattern: " + sessionAttributePattern, e);
+			}
+		}
+	}
 
-      return userDetails;
-    }
-  }
+	public void setTenantedPrincipleNameResolver(ITenantedPrincipleNameResolver tenantedPrincipleNameResolver) {
+		this.tenantedPrincipleNameResolver = tenantedPrincipleNameResolver;
+	}
 
-  @Override
-  public UserDetails loadUserByUsername( String user ) throws UsernameNotFoundException {
+	private List<String> extractValues(List<XMLObject> aValues) {
+		List<String> result = new ArrayList<>();
+		for (XMLObject val : aValues) {
+			String strval = null;
+			if (val instanceof XSString) {
+				strval = ((XSString) val).getValue();
+			} else if (val instanceof XSAny) {
+				strval = ((XSAny) val).getTextContent();
+			}
+			if (strval != null) {
+				result.add(strval);
+			}
+		}
+		return result;
+	}
 
-    if ( user != null ) {
+	private void loadSessionAttributes(IPentahoSession session, List<Attribute> attributes, Pattern pattern, String aRoleAttribute) {
+		for (Attribute attr : attributes) {
+			String attrName = attr.getName();
+			if (!attrName.equals(aRoleAttribute) && pattern.matcher(attrName).matches()) {
+				List<String> values = this.extractValues(attr.getAttributeValues());
+				if (values.size() == 1) {
+					session.setAttribute(attr.getName(), values.get(0));
+					logger.debug(String.format("Setting session attribute [%30s] : %s", attr.getName(), values.get(0)));
+				} else if (!values.isEmpty()) {
+					session.setAttribute(attr.getName(), values);
+					logger.debug(String.format("Setting session attribute [%30s] : %s", attr.getName(), values));
+				}
+			}
+		}
+	}
 
-      if ( getTenantedPrincipleNameResolver() != null ) {
-        user = getTenantedPrincipleNameResolver().getPrincipleName( user );
-      }
+	private Collection<GrantedAuthority> roleRelatedAttributeToGrantedAuthorities(Attribute roleRelatedAttribute) {
 
-      UserDetails userDetails = Utils.getUserMap().get( user );
-      if ( userDetails != null ) {
-        return userDetails;
-      }
-    }
-    throw new UsernameNotFoundException( user );
-  }
+		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 
-  public String getRoleRelatedUserAttributeName() {
-    return roleRelatedUserAttributeName;
-  }
+		if (roleRelatedAttribute != null && roleRelatedAttribute.hasChildren()) {
 
-  public void setRoleRelatedUserAttributeName( String roleRelatedUserAttributeName ) {
-    this.roleRelatedUserAttributeName = roleRelatedUserAttributeName;
-  }
+			List<XMLObject> roleXmlValues = roleRelatedAttribute.getAttributeValues();
 
-  public String getRoleRelatedAttributePrefix() {
-    return roleRelatedAttributePrefix;
-  }
+			for (XMLObject xmlValue : roleXmlValues) {
 
-  public void setRoleRelatedAttributePrefix( String roleRelatedAttributePrefix ) {
-    this.roleRelatedAttributePrefix = roleRelatedAttributePrefix;
-  }
+				if (xmlValue == null) {
+					logger.warn("Empty value for attribute '" + roleRelatedAttribute + "'");
+					continue;
+				}
 
-  public String getSessionAttributePattern() {
-    return sessionAttributePattern;
-  }
+				String sanitizedValue = "";
 
-  public void setSessionAttributePattern(String aSessionAttributePattern) {
-    sessionAttributePattern = aSessionAttributePattern;
-    if (sessionAttributePattern != null && !sessionAttributePattern.isEmpty()) {
-      try {
-        Pattern.compile(sessionAttributePattern);
-      } catch (PatternSyntaxException e) {
-        throw new IllegalArgumentException("Not a valid session attribute pattern: " + sessionAttributePattern, e);
-      }
-    }
-  }
+				if (xmlValue instanceof XSStringImpl) {
+					sanitizedValue = ((XSStringImpl) xmlValue).getValue();
+				} else if (xmlValue instanceof XSAnyImpl) {
+					sanitizedValue = ((XSAnyImpl) xmlValue).getTextContent();
+				} else {
+					logger.warn("Unknown attribute type: " + xmlValue.toString());
+					continue;
+				}
 
-  public ITenantedPrincipleNameResolver getTenantedPrincipleNameResolver() {
-    return tenantedPrincipleNameResolver;
-  }
+				// if there's a prefix configured, check it ( and trim it )
+				if (getRoleRelatedAttributePrefix() != null && !getRoleRelatedAttributePrefix().isEmpty()
+						&& sanitizedValue.startsWith(getRoleRelatedAttributePrefix())) {
 
-  public void setTenantedPrincipleNameResolver( ITenantedPrincipleNameResolver tenantedPrincipleNameResolver ) {
-    this.tenantedPrincipleNameResolver = tenantedPrincipleNameResolver;
-  }
+					sanitizedValue = sanitizedValue.replace(getRoleRelatedAttributePrefix(), "");
+				}
 
-  private Collection<GrantedAuthority> commaSeparatedRoleListToGrantedAuthorities( String commaSeparatedRoleList ) {
+				// finally, add it to the authorities list as a granted authority
+				if (sanitizedValue != null && !sanitizedValue.isEmpty()) {
+					authorities.add(new SimpleGrantedAuthority(sanitizedValue));
+				}
+			}
+		}
 
-    Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-
-    if( commaSeparatedRoleList != null && commaSeparatedRoleList.length() > 0 ){
-
-      if( commaSeparatedRoleList.contains( "," ) ){
-
-        for ( String role : commaSeparatedRoleList.split( "," ) ) {
-          authorities.add( new SimpleGrantedAuthority( role ) );
-        }
-
-      } else {
-
-        authorities.add( new SimpleGrantedAuthority( commaSeparatedRoleList ) );
-      }
-    }
-
-    return authorities;
-  }
-
-  private void loadSessionAttributes(IPentahoSession session, List<Attribute> attributes, Pattern pattern, String aRoleAttribute) {
-    for ( Attribute attr : attributes ) {
-      final String attrName = attr.getName();
-      if (attrName.equals(aRoleAttribute) || ! pattern.matcher(attrName).matches()) {
-        continue;
-      }
-      List<String> values = extractValues(attr.getAttributeValues());
-      if ( values.size() == 1 ) {
-        session.setAttribute(attr.getName(), values.get(0));
-      } else if (!values.isEmpty()) {
-        session.setAttribute(attr.getName(), values);
-      }
-    }
-  }
-
-  private List<String> extractValues(List<XMLObject> aValues) {
-    List<String> result = new ArrayList<>();
-    for (XMLObject val : aValues) {
-      String strval = null;
-      if ( val instanceof XSString ) {
-        strval = ((XSString) val).getValue();
-      } else if ( val instanceof XSAny ) {
-        strval = ((XSAny) val).getTextContent();
-      }
-      if (strval != null) {
-        result.add(strval);
-      }
-    }
-    return result;
-  }
-
-  private Collection<GrantedAuthority> roleRelatedAttributeToGrantedAuthorities( Attribute roleRelatedAttribute ) {
-
-    Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-
-    if( roleRelatedAttribute != null && roleRelatedAttribute.hasChildren() ){
-
-      List<XMLObject> roleXmlValues = roleRelatedAttribute.getAttributeValues();
-
-      for( XMLObject xmlValue : roleXmlValues ) {
-
-        if( xmlValue == null ){
-          logger.warn( "Empty value for attribute '" + roleRelatedAttribute + "'" );
-          continue;
-        }
-
-        String sanitizedValue = "";
-
-        if( xmlValue instanceof XSStringImpl ) {
-          sanitizedValue = ( ( XSStringImpl ) xmlValue ).getValue();
-        } else if( xmlValue instanceof XSAnyImpl ) {
-          sanitizedValue = ( ( XSAnyImpl ) xmlValue ).getTextContent();
-        } else {
-          logger.warn( "Unknown attribute type: " + xmlValue.toString() );
-          continue;
-        }
-
-        // if there's a prefix configured, check it ( and trim it )
-        if( getRoleRelatedAttributePrefix() != null && !getRoleRelatedAttributePrefix().isEmpty()
-            && sanitizedValue.startsWith( getRoleRelatedAttributePrefix() ) ){
-
-          sanitizedValue = sanitizedValue.replace( getRoleRelatedAttributePrefix(), "" );
-        }
-
-        // finally, add it to the authorities list as a granted authority
-        if( sanitizedValue != null && !sanitizedValue.isEmpty() ) {
-          authorities.add( new SimpleGrantedAuthority( sanitizedValue ) );
-        }
-      }
-    }
-
-    return authorities;
-  }
+		return authorities;
+	}
 }
